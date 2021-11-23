fff04cc00ca13dc0b6386fbbbe8b2ebb05b2253c
Kornelia Zarębińska <71494339+Tykutyku@users.noreply.github.com>
2021-11-23 12:55:02 +0100

    Update storage.js

--- storage.js
+++ storage.js
@@ -7,8 +7,10 @@ class Storage{
 	
 	read(title){
 			const books = fs.readFileSync(this.path);
+            parseBook = JSON.parse(books.toString());
 			console.log(books);
 		}
-}
+       
+    }
 
 module.exports = Storage;
